a1d0d5ec92eb1db373d4a07176c26abbe191eca0
Mentabomber <mentae_mentology@libero.it>
2023-12-14 12:28:54 +0100

    auth controller/validation/route

---
+++ back-end/routers/auth.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const router = Router();
+const authController = require("../controllers/auth");
+const { checkSchema } = require("express-validator");
+const { checkValidity } = require("../middlewares/schemaValidator");
+const userRegister = require("../validations/userRegister");
+const userLogin = require("../validations/userLogin");
+
+router.post(
+    "/register",
+    checkSchema(userRegister),
+    checkValidity,
+    authController.register
+);
+router.post(
+    "/login",
+    checkSchema(userLogin),
+    checkValidity,
+    authController.login
+);
+
+module.exports = router;
