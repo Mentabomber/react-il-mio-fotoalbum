948132a190a3d8c9a4e2ced241fc13baec5e38b6
Mentabomber <mentae_mentology@libero.it>
2023-12-15 12:32:18 +0100

    formatting

--- back-end/middlewares/authHandler.js
+++ back-end/middlewares/authHandler.js
@@ -20,7 +20,7 @@ module.exports = (req, res, next) => {
   // estraggo il token
   const token = bearer.split(" ")[1];
 
-  // verificare che il token sia valido. 
+  // verificare che il token sia valido.
   // Il verify da solo lancia degli errori in caso di token non valido o scaduto
   // estrarre i dati dell'utente dal token. Il verify ritorna il payload del token, quindi l'utente
   const user = jsonwebtoken.verify(token, process.env.JWT_SECRET);
--- back-end/middlewares/userIdHandler.js
+++ back-end/middlewares/userIdHandler.js
@@ -1,4 +1,3 @@
-
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const WrongUser = require("../exceptions/WrongUser");
@@ -11,39 +10,34 @@ const jsonwebtoken = require("jsonwebtoken");
  * @param {*} next
  */
 module.exports = async (req, res, next) => {
+  const bearer = req.headers.authorization;
 
-        const bearer = req.headers.authorization;
-  
-        const token = bearer.split(" ")[1];
-        const user = jsonwebtoken.verify(token, process.env.JWT_SECRET);
-        const showInputData = req.params;
-        const showPhoto = await prisma.photo
-        .findUnique({
-            where: {
-                id: parseInt(showInputData.id),
-            },
-            include: {
-                categories: {
-                    select: {
-                        type: true,
-                        description: true,
-                    },
-                },
-                user: true,
-            }
+  const token = bearer.split(" ")[1];
+  const user = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+  const showInputData = req.params;
+  const showPhoto = await prisma.photo.findUnique({
+    where: {
+      id: parseInt(showInputData.id),
+    },
+    include: {
+      categories: {
+        select: {
+          type: true,
+          description: true,
+        },
+      },
+      user: true,
+    },
+  });
+  if (!showPhoto) {
+    throw new Error("Not found");
+  }
 
-    });
-    if (!showPhoto) {
-        throw new Error("Not found");
-    }
-
-
-    if (!(showPhoto.user.id === user.id)) {
-        throw new WrongUser(
-        "Non puoi cancellare questa foto perché non sei l'utente che la ha creata"
+  if (!(showPhoto.user.id === user.id)) {
+    throw new WrongUser(
+      "Non puoi cancellare questa foto perché non sei l'utente che la ha creata"
     );
-    }
-    // Continue to the next middleware or route handler
-    next();
-
+  }
+  // Continue to the next middleware or route handler
+  next();
 };
